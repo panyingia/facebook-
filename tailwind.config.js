38839f8c78f2652de4b03884e2fe31337bf481d3
panyingia <panyingia@gmail.com>
2022-08-22 19:54:41 +0800

    8月22日晚上7點

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,23 @@
 module.exports = {
   content: ['./src/**/*.html'],
   mode: 'jit',
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
         'Poppins': ['Poppins', 'sans-serif']
       },
+      spacing: {
+        'main-span': '72px'
+      },
       colors: {
-        'primary': '#4f3cc9',
-        'primary-dark': '#4232aa',
-        'secondary': '#ffffff'
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#3e4042',
+        'fb-active': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff'
       },
     },
   },
